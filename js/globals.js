3c03d75bbd07304dc54e2b617756dab1b9db6d14
nickubed <nhpquandt@gmail.com>
2019-12-04 16:43:59 -0800

    Updated starting health to live

--- js/globals.js
+++ js/globals.js
@@ -5,7 +5,7 @@ const GAME_WIDTH = 350
 const LEVEL_INCREMENT = 50
 const SCORE_SLOTS = 3
 const SPAWN_COORD = [null, 70, 140, 210, 280]
-const STARTING_LIFE = 1
+const STARTING_LIFE = 5
 const SWITCH_EMOJI_DELAY = 0.2
 const USER_SPEED = 220
 
