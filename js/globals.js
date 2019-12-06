37114cba929a45f087fcc2f71bf05f7a145b263f
nickubed <nhpquandt@gmail.com>
2019-12-05 16:22:40 -0800

    Updated HTML

--- js/globals.js
+++ js/globals.js
@@ -4,7 +4,6 @@ const EMOJI = []
 const GAME_HEIGHT = 623
 const GAME_WIDTH = 350
 const LEVEL_INCREMENT = 50
-const SCORE_SLOTS = 3
 const SPAWN_COORD = [0, 70, 140, 210, 280]
 const STARTING_LIFE = 5
 const SWITCH_EMOJI_DELAY = 0.2
